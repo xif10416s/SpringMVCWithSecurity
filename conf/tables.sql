a041ee21f73d92dd0084de14294a5063dae1681c
席斐 <seki@xifeideMacBook-Pro.local>
2016-05-09 00:18:41 +0800

    111

---
+++ conf/tables.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `persistent_logins` (
+  `username` varchar(64) NOT NULL,
+  `series` varchar(64) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `last_used` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`series`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
